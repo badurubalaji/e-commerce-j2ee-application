f77f9b712eeb0dc559f4c5f33f40f1cb35e6971f
ndkhoa.is@gmail.com <ndkhoa.is@gmail.com@5483ff32-1bfb-d7a7-664b-383909b78ef7>
2010-07-31 14:50:46 +0000

    Model Stage : Accessing Data with EJBs - Fix yesterday's problem - Problem is configuration ControllerServlet using Annotation (auto-init when app runs) or using XML-config (web.xml - not auto-init so that index page cannot be shown correctly)

--- AffableBean/src/java/controller/ControllerServlet.java
+++ AffableBean/src/java/controller/ControllerServlet.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,6 +21,15 @@ import session.ProductFacade;
  *
  * @author dangkhoa
  */
+@WebServlet(name = "Controller",
+            loadOnStartup = 1,
+            urlPatterns = {"/category",
+                           "/addToCart",
+                           "/viewCart",
+                           "/updateCart",
+                           "/checkout",
+                           "/purchase",
+                           "/chooseLanguage"})
 public class ControllerServlet extends HttpServlet {
 
     @EJB
